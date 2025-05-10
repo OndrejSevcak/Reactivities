6eec5b2dd6511ae7ed1f1bb7a1607b7dc7c60519
OndrejS <ondrejsevcak@gmail.com>
2025-05-10 15:52:04 +0200

    extracting form data from event using FormData API

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -6,21 +6,33 @@ type Props = {
 }
 
 export default function ActivityForm({ onFormClose, activity }: Props) {
+    
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget); //built-in browser API that allows you to easily construct a set of key/value pairs representing form fields and their values.
+        const data: {[key: string]: FormDataEntryValue} = {}    //each key is a string and each value is a FormDataEntryValue (which can be a string or a File object)
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+        console.log(formData);
+        console.log(data);
+    }
+
     return (
         <Paper sx={{ borderRadius: 3, padding: 3 }}>
             <Typography variant="h5" gutterBottom color="primary">
                 Create activity
             </Typography>
-            <Box component="form" display="flex" flexDirection="column" gap={3}>
-                <TextField label="Title" />
-                <TextField label="Description" multiline rows={3} />
-                <TextField label="Category" />
-                <TextField label="Date" type="date" />
-                <TextField label="City" />
-                <TextField label="Venue" />
+            <Box component="form" onSubmit={handleFormSubmit} display="flex" flexDirection="column" gap={3}>
+                <TextField name="title" label="Title" defaultValue={activity?.title} />
+                <TextField name="description" label="Description" defaultValue={activity?.description} multiline rows={3} />
+                <TextField name="category" label="Category" defaultValue={activity?.category} />
+                <TextField name="date" label="Date" type="date" defaultValue={activity?.date} />
+                <TextField name="city" label="City" defaultValue={activity?.city} />
+                <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
                 <Box display="flex" justifyContent="end" gap={2}>
                     <Button color="inherit" onClick={onFormClose}>Cancel</Button>
-                    <Button color="success" variant="contained">Submit</Button>
+                    <Button type="submit" color="success" variant="contained">Submit</Button>
                 </Box>
             </Box>            
         </Paper>
